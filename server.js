5089586ecf520f40c7af83aaaeb8aa4e9d2c9d34
SAWA622 <78275644+SAWA622@users.noreply.github.com>
2024-05-01 00:34:27 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-// server.js
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
@@ -9,7 +9,7 @@ app.use(express.json());
 
 app.use(cors());
 
-require("dotenv").config();
+
 
 mongoose.connect(process.env.DB_URI, {
   useNewUrlParser: true,
@@ -18,14 +18,12 @@ mongoose.connect(process.env.DB_URI, {
 .then(() => console.log("Database connected"))
 .catch((err) => console.error(err));
 
-// Import routes
 const authRoutes = require("./Routes/auth");
 const breweryRoutes = require("./Routes/brewery");
 
 app.use("/api/auth", authRoutes);
 app.use("/api/brewery", breweryRoutes);
 
-// Define port number
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
